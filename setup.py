8c1e2ff74e7615db8125a0c41686e7b529adb2ab
Umeaiman Merchant <52196824+umeaimanMerchant@users.noreply.github.com>
2024-01-30 16:53:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import find_packages,setup
 
-HYPHEN_DOT_E='-e .'
-def get_requirements(file_path:str)->list[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ") for req in requirements]
-        if HYPHEN_DOT_E in requirements:
-            requirements.remove(HYPHEN_DOT_E)
-    return requirements    
-
-
-
 setup(
     name='Job_Recommendation_System',
     version='0.0.1',
